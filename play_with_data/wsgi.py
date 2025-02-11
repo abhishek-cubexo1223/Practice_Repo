1bca416d6b6e07aa9d9a772953a6decb28513e42
Vijay Goyal <goyalvijay928@gmail.com>
2025-02-11 18:50:58 +0530

    new 1111 commit in new branch

--- play_with_data/wsgi.py
+++ play_with_data/wsgi.py
@@ -22,3 +22,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'play_with_data.settings')
 application = get_wsgi_application()
 # this commit is a new branch
 # this commit is a new branch
+# this commit is a new branch
+# this commit is a new branch
+# this commit is a new branch

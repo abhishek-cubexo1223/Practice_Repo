b3fc5ea4e0891020b85423f4a9c299c767ccfe0f
Vijay Goyal <goyalvijay928@gmail.com>
2025-02-11 18:33:48 +0530

    first commit in main branch

--- play_with_data/wsgi.py
+++ play_with_data/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+# this comment is add for main testing
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'play_with_data.settings')
